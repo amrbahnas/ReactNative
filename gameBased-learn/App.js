5b29119988b3ee8d14527c80af3d844926f560c0
Abdullah Saeed <86889145+abdullah0saeed@users.noreply.github.com>
2022-11-08 14:31:15 +0200

    Add files via upload

---
+++ gameBased-learn/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import styles from "./src/styles";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Start from "./src/screens/Start";
+import Game from "./src/screens/Game";
+
+const stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StatusBar backgroundColor="#000" style="light" />
+      <stack.Navigator>
+        <stack.Screen
+          name="Start"
+          component={Start}
+          options={{ headerShown: false }}
+        />
+        <stack.Screen name="Game" component={Game} />
+      </stack.Navigator>
+    </NavigationContainer>
+  );
+}
